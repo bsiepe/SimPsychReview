c0a23195014eb2d6f0cf39985a96bda695039d92
FBartos <f.bartos96@gmail.com>
2023-10-02 10:15:27 +0200

    Update simulation_example.R

--- simulation-example/simulation_example.R
+++ simulation-example/simulation_example.R
@@ -4,22 +4,25 @@ library(SimDesign)
 
 Design <- createDesign(
   treatment_effect     = c(0.0, 0.2, 0.5),
-  pre_post_correlation = c(0.0, 0.5, 0.7)
+  pre_post_correlation = c(0.0, 0.5, 0.7),
+  n                    = 50
 )
 
 #-------------------------------------------------------------------
 
 Generate <- function(condition, fixed_objects = NULL) {
   
-  Attach(condition)
+  treatment_effect     <- condition[["treatment_effect"]]
+  pre_post_correlation <- condition[["pre_post_correlation"]]
+  n                    <- condition[["n"]]
   
   covariance_matrix <- matrix(c(
     1,                    pre_post_correlation,
     pre_post_correlation,                    1
   ), nrow = 2, ncol = 2, byrow = TRUE)
   
-  treatment_control      <- MASS::mvrnorm(n = 50, mu = c(0, 0),                Sigma = covariance_matrix)
-  treatment_experimental <- MASS::mvrnorm(n = 50, mu = c(0, treatment_effect), Sigma = covariance_matrix)
+  treatment_control      <- MASS::mvrnorm(n = n, mu = c(0, 0),                Sigma = covariance_matrix)
+  treatment_experimental <- MASS::mvrnorm(n = n, mu = c(0, treatment_effect), Sigma = covariance_matrix)
   
   dat <- rbind(
     data.frame(
@@ -44,22 +47,22 @@ Analyse <- function(condition, dat, fixed_objects = NULL) {
   change_score <- lm(post ~ offset(pre) + treatment, data = dat)
   post_score   <- lm(post ~ treatment, data = dat)
   
-  ANCOVA.summary       <- summary(ANCOVA)$coefficients
-  change_score.summary <- summary(change_score)$coefficients
-  post_score.summary   <- summary(post_score)$coefficients
+  ANCOVA_summary       <- summary(ANCOVA)$coefficients
+  change_score_summary <- summary(change_score)$coefficients
+  post_score_summary   <- summary(post_score)$coefficients
   
   ret <- nc(
-    ANCOVA.est    = ANCOVA.summary["treatmentexperimental", "Estimate"],
-    ANCOVA.est_se = ANCOVA.summary["treatmentexperimental", "Std. Error"],
-    ANCOVA.pval   = ANCOVA.summary["treatmentexperimental", "Pr(>|t|)"],
+    ANCOVA_est    = ANCOVA_summary["treatmentexperimental", "Estimate"],
+    ANCOVA_est_se = ANCOVA_summary["treatmentexperimental", "Std. Error"],
+    ANCOVA_pval   = ANCOVA_summary["treatmentexperimental", "Pr(>|t|)"],
     
-    change_score.est    = change_score.summary["treatmentexperimental", "Estimate"],
-    change_score.est_se = change_score.summary["treatmentexperimental", "Std. Error"],
-    change_score.pval   = change_score.summary["treatmentexperimental", "Pr(>|t|)"],
+    change_score_est    = change_score_summary["treatmentexperimental", "Estimate"],
+    change_score_est_se = change_score_summary["treatmentexperimental", "Std. Error"],
+    change_score_pval   = change_score_summary["treatmentexperimental", "Pr(>|t|)"],
     
-    post_score.est    = post_score.summary["treatmentexperimental", "Estimate"],
-    post_score.est_se = post_score.summary["treatmentexperimental", "Std. Error"],
-    post_score.pval   = post_score.summary["treatmentexperimental", "Pr(>|t|)"]
+    post_score_est    = post_score_summary["treatmentexperimental", "Estimate"],
+    post_score_est_se = post_score_summary["treatmentexperimental", "Std. Error"],
+    post_score_pval   = post_score_summary["treatmentexperimental", "Pr(>|t|)"]
   )
   
   return(ret)
@@ -70,27 +73,27 @@ Summarise <- function(condition, results, fixed_objects = NULL) {
   Attach(condition)
   Attach(results)
   
-  EDR_MCSE  <- function(pval) sqrt(EDR(pval) * (1-EDR(pval)) / length(pval))
+  EDR_MCSE  <- function(pval) sqrt(EDR(pval) * (1 - EDR(pval)) / length(pval))
   bias_MCSE <- function(est)  var(est) / sqrt(length(est))
   
   ret <- c(
-    ANCOVA.EDR             = EDR(ANCOVA.pval),
-    ANCOVA.EDR_MCSE        = EDR_MCSE(ANCOVA.pval),
-    ANCOVA.bias            = bias(ANCOVA.est, treatment_effect),
-    ANCOVA.bias_MCSE       = bias_MCSE(ANCOVA.est),
-    ANCOVA.est.var         = var(ANCOVA.est),
+    ANCOVA_EDR             = EDR(ANCOVA_pval),
+    ANCOVA_EDR_MCSE        = EDR_MCSE(ANCOVA_pval),
+    ANCOVA_bias            = bias(ANCOVA_est, treatment_effect),
+    ANCOVA_bias_MCSE       = bias_MCSE(ANCOVA_est),
+    ANCOVA_est_var         = var(ANCOVA_est),
     
-    change_score.EDR       = EDR(change_score.pval),
-    change_score.EDR_MCSE  = EDR_MCSE(change_score.pval),
-    change_score.bias      = bias(change_score.est, treatment_effect),
-    change_score.bias_MCSE = bias_MCSE(change_score.est),
-    change_score.est.var   = var(change_score.est),
+    change_score_EDR       = EDR(change_score_pval),
+    change_score_EDR_MCSE  = EDR_MCSE(change_score_pval),
+    change_score_bias      = bias(change_score_est, treatment_effect),
+    change_score_bias_MCSE = bias_MCSE(change_score_est),
+    change_score_est_var   = var(change_score_est),
     
-    post_score.EDR         = EDR(post_score.pval),
-    post_score.EDR_MCSE    = EDR_MCSE(post_score.pval),
-    post_score.bias        = bias(post_score.est, treatment_effect),
-    post_score.bias_MCSE   = bias_MCSE(post_score.est),
-    post_score.est.var     = var(post_score.est)
+    post_score_EDR         = EDR(post_score_pval),
+    post_score_EDR_MCSE    = EDR_MCSE(post_score_pval),
+    post_score_bias        = bias(post_score_est, treatment_effect),
+    post_score_bias_MCSE   = bias_MCSE(post_score_est),
+    post_score_est_var     = var(post_score_est)
   )
   
   return(ret)
@@ -107,6 +110,6 @@ res_pilot <- runSimulation(
   summarise    = Summarise
 )
 
-res_pilot$ANCOVA.est.var       / 0.001^2
-res_pilot$change_score.est.var / 0.001^2
-res_pilot$post_score.est.var   / 0.001^2
+res_pilot$ANCOVA_est_var       / 0.005^2
+res_pilot$change_score_est_var / 0.005^2
+res_pilot$post_score_est_var   / 0.005^2
